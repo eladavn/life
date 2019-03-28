f1d477cb207a0210f683dafdb5d8bbd946384dd5
Elad Avneri <elad.avneri@Admins-MacBook-Pro.local>
2019-03-28 21:29:09 +0200

    GREEN: should keep a cell with two neighbors

--- getNextGen.ts
+++ getNextGen.ts
@@ -1,5 +1,6 @@
 import * as ndarray from 'ndarray';
 import {List} from 'immutable';
+import * as _ from 'lodash';
 
 export function getNextGen(currGen : ndarray<number> ) : ndarray<number> | Error {
 
@@ -14,12 +15,19 @@ function convert2dto1dIndex(row:number, col:number) : number {
     return 4;   
 }
 
+function shouldCellLive(currGen : ndarray<number>, row:number, col:number) : boolean {
+    return _.sum(currGen.data)-1 < 2 ? false : true;
+}
+
 function getNextGen2D(currGen : ndarray<number> ) : ndarray<number> {
 
-    return ndarray( currGen.shape[0] < 3 || currGen.shape[1] < 3 ?
-                currGen.data.slice() :
-                List<number>(currGen.data).update(convert2dto1dIndex(1,1), _ => 0).toArray()
-            ,currGen.shape
-    );
+    return ndarray( 
+        currGen.shape[0] < 3 || currGen.shape[1] < 3 ?
+            currGen.data.slice() :
+            List<number>(currGen.data)
+                .update(convert2dto1dIndex(1,1),
+                    _ =>  shouldCellLive(currGen,1,1) ? 1 : 0)
+                .toArray()
+        ,currGen.shape);
 
 }
