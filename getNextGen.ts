59bc033a6913f5d3709d564302a0e37d38ef808a
Elad Avneri <elad.avneri@Admins-MacBook-Pro.local>
2019-03-28 11:29:21 +0200

    GREEN: should accept up to two dimensions

--- getNextGen.ts
+++ getNextGen.ts
@@ -1,6 +1,11 @@
 import * as ndarray from 'ndarray';
 
 export function getNextGen(currGen : ndarray<boolean> ) : ndarray<boolean> {
+
+    if (currGen.dimension > 2) {
+        throw new Error('Should be called with less than 3 dimensions');
+    }
+
     if (currGen.data.length === 0) {
         return ndarray<boolean>([]);
     }
