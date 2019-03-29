bd67f9c56ecf7e2bcbe7128b36a0afe5ec7503c5
Elad Avneri <elad.avneri@Admins-MacBook-Pro.local>
2019-03-29 12:41:24 +0300

    GREEN: getNumLivesAroundCell with cell on right edge

--- getNumLivesAroundCell.ts
+++ getNumLivesAroundCell.ts
@@ -2,8 +2,11 @@ import * as ndarray from 'ndarray';
 import * as _ from 'lodash';
 let ops = require('ndarray-ops');
 
+function getNumColumns(matrix : ndarray<number>) : number {
+    return matrix.shape[1];
+}
 
 export function getNumLivesAroundCell(matrix : ndarray<number>, row:number, col:number) : number {
 
-    return ops.sum(matrix.lo(row-1,col-1).hi(3,3)) - matrix.get(row,col);
+    return ops.sum(matrix.lo(row-1,col-1).hi(3, col === getNumColumns(matrix)-1 ? 2 : 3 ) ) - matrix.get(row,col);
 }
