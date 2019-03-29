f58e91642591414e3a77f3649c9cfa4a12421181
Elad Avneri <elad.avneri@Admins-MacBook-Pro.local>
2019-03-29 09:46:41 +0300

    GREEN : getNumLivesAroundCell with cell surrounded in all directions

--- getNumLivesAroundCell.ts
+++ getNumLivesAroundCell.ts
@@ -1,6 +1,9 @@
 import * as ndarray from 'ndarray';
 import * as _ from 'lodash';
+let ops = require('ndarray-ops');
+
 
 export function getNumLivesAroundCell(matrix : ndarray<number>, row:number, col:number) : number {
-    return _.sum(matrix.data) - matrix.get(1,1);
+
+    return ops.sum(matrix.lo(row-1,col-1).hi(3,3)) - matrix.get(row,col);
 }
