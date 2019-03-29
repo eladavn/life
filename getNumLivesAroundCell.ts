61a2a7d57f4644831f4b8b3f73ca8b2336f046ec
Elad Avneri <elad.avneri@Admins-MacBook-Pro.local>
2019-03-29 12:48:17 +0300

    REFACTOR: getNumLivesAroundCell with cell on right edge

--- getNumLivesAroundCell.ts
+++ getNumLivesAroundCell.ts
@@ -6,7 +6,15 @@ function getNumColumns(matrix : ndarray<number>) : number {
     return matrix.shape[1];
 }
 
+function isOnRightEdge(col : number, matrix: ndarray<number>) : boolean {
+    return col === getNumColumns(matrix)-1;
+}
+
+function getSurroundingWidth(col : number, matrix: ndarray<number>) : number {
+    return isOnRightEdge(col, matrix) ? 2 : 3;
+}
+
 export function getNumLivesAroundCell(matrix : ndarray<number>, row:number, col:number) : number {
 
-    return ops.sum(matrix.lo(row-1,col-1).hi(3, col === getNumColumns(matrix)-1 ? 2 : 3 ) ) - matrix.get(row,col);
+    return ops.sum(matrix.lo(row-1,col-1).hi(3, getSurroundingWidth(col,matrix) ) ) - matrix.get(row,col);
 }
