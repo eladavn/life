e8234823532418c3af299556cb5b1f1337cb86dc
Elad Avneri <elad.avneri@Admins-MacBook-Pro.local>
2019-03-30 21:27:38 +0300

    GREEN: getNumLivesAroundCell with cell on left edge

--- getNumLivesAroundCell.ts
+++ getNumLivesAroundCell.ts
@@ -6,15 +6,27 @@ function getNumColumns(matrix : ndarray<number>) : number {
     return matrix.shape[1];
 }
 
+
+function isOnLeftEdge(col : number, matrix: ndarray<number>) : boolean {
+    return col === 0;
+}
+
 function isOnRightEdge(col : number, matrix: ndarray<number>) : boolean {
     return col === getNumColumns(matrix)-1;
 }
 
-function getSurroundingWidth(col : number, matrix: ndarray<number>) : number {
-    return isOnRightEdge(col, matrix) ? 2 : 3;
+function getSurroundingLeftCol(col : number, matrix: ndarray<number>) : number {
+    return isOnLeftEdge(col, matrix) ? 0 : col-1;
 }
+
+function getSurroundingWidth(col : number, matrix: ndarray<number>) : number {
+    return isOnLeftEdge(col, matrix) || isOnRightEdge(col, matrix) ? 2 : 3;
+}
+
 
 export function getNumLivesAroundCell(matrix : ndarray<number>, row:number, col:number) : number {
 
-    return ops.sum(matrix.lo(row-1,col-1).hi(3, getSurroundingWidth(col,matrix) ) ) - matrix.get(row,col);
+    return ops.sum(matrix
+                .lo(row-1,getSurroundingLeftCol(col,matrix))
+                .hi(3, getSurroundingWidth(col,matrix) ) ) - matrix.get(row,col);
 }
