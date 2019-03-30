52986319aa3182e22c24ac04bc03fcf2ebda3f58
Elad Avneri <elad.avneri@Admins-MacBook-Pro.local>
2019-03-30 23:32:18 +0300

    GREEN: getNextGen should iterate on a single row'

--- getNextGen.ts
+++ getNextGen.ts
@@ -20,11 +20,19 @@ function shouldCellLive(currGen : ndarray<number>, row:number, col:number) : boo
     return getNumLivesAroundCell(currGen,row,col) < 2 ? false : true;
 }
 
+function getNextGenCell(currGen : ndarray<number>, row:number, col:number) : number {
+    return shouldCellLive(currGen,row,col) ? 1 : 0;
+}
+
+function  getNextGenForLessThanTwoRowsMatrix(currGen : ndarray<number>) : ndarray.Data<number> {
+    return currGen.data.map((val : number, index : number) => getNextGenCell(currGen, 0, index));
+}
+
 function getNextGen2D(currGen : ndarray<number> ) : ndarray<number> {
 
     return ndarray( 
-        currGen.shape[0] < 3 || currGen.shape[1] < 3 ?
-            currGen.data.slice() :
+        currGen.shape[0] < 2 ?
+            getNextGenForLessThanTwoRowsMatrix(currGen) :
             List<number>(currGen.data)
                 .update(convert2dto1dIndex(1,1),
                     _ =>  shouldCellLive(currGen,1,1) ? 1 : 0)
