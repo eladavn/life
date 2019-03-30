bc5e6f195d47e619a46a0ba316a13946a15e2b3f
Elad Avneri <elad.avneri@Admins-MacBook-Pro.local>
2019-03-30 22:10:56 +0300

    GREEN: getNumLivesAroundCell with cell on top edge

--- getNumLivesAroundCell.ts
+++ getNumLivesAroundCell.ts
@@ -6,6 +6,9 @@ function getNumColumns(matrix : ndarray<number>) : number {
     return matrix.shape[1];
 }
 
+function isOnTopEdge(row : number, matrix: ndarray<number>) : boolean {
+    return row === 0;
+}
 
 function isOnLeftEdge(col : number, matrix: ndarray<number>) : boolean {
     return col === 0;
@@ -19,14 +22,23 @@ function getSurroundingLeftCol(col : number, matrix: ndarray<number>) : number {
     return isOnLeftEdge(col, matrix) ? 0 : col-1;
 }
 
+function getSurroundingTopRow(row : number, matrix: ndarray<number>) : number {
+    return isOnTopEdge(row, matrix) ? 0 : row-1;
+}
+
 function getSurroundingWidth(col : number, matrix: ndarray<number>) : number {
     return isOnLeftEdge(col, matrix) || isOnRightEdge(col, matrix) ? 2 : 3;
+}
+
+function getSurroundingHeight(row : number, matrix: ndarray<number>) : number {
+    return isOnTopEdge(row, matrix)  ? 2 : 3;
 }
 
 
 export function getNumLivesAroundCell(matrix : ndarray<number>, row:number, col:number) : number {
 
     return ops.sum(matrix
-                .lo(row-1,getSurroundingLeftCol(col,matrix))
-                .hi(3, getSurroundingWidth(col,matrix) ) ) - matrix.get(row,col);
+                .lo(getSurroundingTopRow(row,matrix),getSurroundingLeftCol(col,matrix))
+                .hi(getSurroundingHeight(row,matrix), getSurroundingWidth(col,matrix) ) ) 
+            - matrix.get(row,col);
 }
