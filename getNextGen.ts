8ddcedae5d872d4fbb9d93359899ff0717521dd6
Elad Avneri <elad.avneri@Admins-MacBook-Pro.local>
2019-03-31 09:36:38 +0300

    REFACTOR

--- getNextGen.ts
+++ getNextGen.ts
@@ -1,68 +1,43 @@
 import * as ndarray from 'ndarray';
 import {getNumLivesAroundCell} from './getNumLivesAroundCell';
 
-let shouldDebug = false;
-
 export function getNextGen(currGen : ndarray<number> ) : ndarray<number> | Error {
 
     return (
         currGen.dimension > 2 ?
             new Error('Should be called with less than 3 dimensions') :
-            getNextGen2D(currGen)            
-    );
+            getNextGen2D(
+                currGen.shape.length === 1? 
+                    // Incase of a flat array make a 1x1 matrix of it
+                    ndarray(currGen.data, [1,1]) : 
+                    currGen)
+        );
 }
 
 function getRowFromFlatArrayIndex(index:number, colsCount : number) : number {
-
-    if (shouldDebug) {
-        console.log(`getRowFromFlatArrayIndex [${index},${colsCount}] : ${Math.floor(index/colsCount)}`);
-    }
 
     return Math.floor(index/colsCount);   
 }
 
 function getColFromFlatArrayIndex(index:number, colsCount : number) : number {
-
-    if (shouldDebug) {
-        console.log(`getColFromFlatArrayIndex [${index},${colsCount}] : ${index%colsCount}`);
-    }
-
     return index%colsCount;   
 }
 
 
 function shouldCellLive(currGen : ndarray<number>, row:number, col:number) : boolean {
+
     return getNumLivesAroundCell(currGen,row,col) < 2 ? false : true;
 }
 
 function getNextGenCell(currGen : ndarray<number>, row:number, col:number) : number {
-
-    if (shouldDebug) {
-        console.log(`getNextGenCel [${row},${col}]`);
-    }
-
     return shouldCellLive(currGen,row,col) ? 1 : 0;
-}
-
-function  getNextGenForLessThanTwoRowsMatrix(currGen : ndarray<number>) : ndarray.Data<number> {
-    return currGen.data.map((val : number, index : number) => getNextGenCell(currGen, 0, index));
-}
-
-function  getNextGenForMoreThanOneRowsMatrix(currGen : ndarray<number>) : ndarray.Data<number> {
-
-    shouldDebug = true;
-
-    return currGen.data.map((val : number, index : number) => getNextGenCell(currGen, 
-                getRowFromFlatArrayIndex(index, currGen.shape[1]),
-                getColFromFlatArrayIndex(index, currGen.shape[1])));
 }
 
 function getNextGen2D(currGen : ndarray<number> ) : ndarray<number> {
 
-    return ndarray( 
-        currGen.shape[0] < 2 ?
-            getNextGenForLessThanTwoRowsMatrix(currGen) :
-            getNextGenForMoreThanOneRowsMatrix(currGen)            
+    return ndarray(currGen.data.map((val : number, index : number) => getNextGenCell(currGen, 
+            getRowFromFlatArrayIndex(index, currGen.shape[1]),
+            getColFromFlatArrayIndex(index, currGen.shape[1])))            
         ,currGen.shape);
 
 }
